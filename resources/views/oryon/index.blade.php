7e4448c23e56d3531e91d673499bdbd94bef84ef
Lucas Garssa <garssa.lucas@gmail.com>
2025-03-18 23:25:55 -0300

    CustomizeIndexOryon

--- resources/views/oryon/index.blade.php
+++ resources/views/oryon/index.blade.php
@@ -1,40 +1,49 @@
-@extends('oryon.layouts.main')
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Lista de Produtos Oryon') }}
+        </h2>
+    </x-slot>
 
-@section('title', 'Lista de Produtos Oryon')
-
-@section('content')
-
-    <a href="{{ route('oryon.new') }}">Novo Produto</></a>
-        <h1>Lista de Produtos Oryon</h1>
-        <x-alert/>
-        <table border="1">
-            <thead>
-                <tr>
-                    <th>Código</th>
-                    <th>Descrição</th>
-                    <th>Preço</th>
-                    <th>Categoria</th>
-                    <th>Fornecedor</th>
-                    <th>Peso</th>
-                    <th>Preço de Compra</th>
-                    <th>Estoque</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($produtos as $produto)
-                    <tr>
-                        <td>{{ $produto->codigo }}</td>
-                        <td>{{ $produto->descricao }}</td>
-                        <td>{{ number_format($produto->preco, 2, ',', '.') }}</td>
-                        <td>{{ $produto->categoria }}</td>
-                        <td>{{ $produto->fornecedor }}</td>
-                        <td>{{ number_format($produto->peso, 3, ',', '.') }}</td>
-                        <td>{{ number_format($produto->preco_compra, 2, ',', '.') }}</td>
-                        <td>{{ number_format($produto->estoque, 3, ',', '.') }}</td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-        {{$produtos->links()}}
-        <a href="{{ route('oryon.importar') }}">Importar Produtos</a>
-@endsection
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <x-alert />
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <table border="1">
+                        <thead>
+                            <tr>
+                                <th>Código</th>
+                                <th>Descrição</th>
+                                <th>Preço</th>
+                                <th>Categoria</th>
+                                <th>Fornecedor</th>
+                                <th>Peso</th>
+                                <th>Preço de Compra</th>
+                                <th>Estoque</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($produtos as $produto)
+                                <tr>
+                                    <td>{{ $produto->codigo }}</td>
+                                    <td>{{ $produto->descricao }}</td>
+                                    <td>{{ number_format($produto->preco, 2, ',', '.') }}</td>
+                                    <td>{{ $produto->categoria }}</td>
+                                    <td>{{ $produto->fornecedor }}</td>
+                                    <td>{{ number_format($produto->peso, 3, ',', '.') }}</td>
+                                    <td>{{ number_format($produto->preco_compra, 2, ',', '.') }}</td>
+                                    <td>{{ number_format($produto->estoque, 3, ',', '.') }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                    {{$produtos->links()}}
+                    <a href="{{ route('oryon.new') }}">++ Novo Produto</></a>
+                    <br>
+                    <a href="{{ route('oryon.importar') }}"> >> Importar Produtos</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
