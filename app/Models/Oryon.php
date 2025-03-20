bfb29cbe7132ee67e587a0e0ece792c02f468d9d
Lucas Garssa <garssa.lucas@gmail.com>
2025-03-20 13:47:52 -0300

    SoftDeleteOryon

--- app/Models/Oryon.php
+++ app/Models/Oryon.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Oryon extends Model
 {
+    use SoftDeletes;
     protected $table = 'oryons';
     //quais colunas podem ser preenchidas:
     protected $fillable = ['codigo', 'descricao', 'preco', 'categoria', 'fornecedor', 'peso', 'preco_compra', 'estoque']; 
