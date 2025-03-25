b670b81dfc85aef2f1b99c828d11996863bf73bb
Lucas Garssa <garssa.lucas@gmail.com>
2025-03-25 19:18:01 -0300

    JqueryInstall and UpdateOryonIndex

--- resources/views/oryon/index.blade.php
+++ resources/views/oryon/index.blade.php
@@ -4,7 +4,6 @@
             {{ __('Lista de Produtos Oryon') }}
         </h2>
     </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <x-alert />
@@ -41,40 +40,55 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <div class="overflow-x-auto">
-                        <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700 table-none md:table-fixed">
-                            <thead class="bg-gray-50 dark:bg-gray-700">
+                        <table
+                            class="min-w-full divide-y divide-gray-200 dark:divide-gray-700 table-none md:table-fixed">
+                            <thead class="bg-gray-300 dark:bg-gray-700">
                                 <tr>
-                                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">Código</th>
-                                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">Descrição</th>
-                                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">Preço</th>
-                                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">Categoria</th>
-                                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">Fornecedor</th>
-                                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">Peso</th>
-                                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">Preço de Compra</th>
-                                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">Estoque</th>
-                                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">Ações</th>
+                                    <th
+                                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">
+                                        Código</th>
+                                    <th
+                                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">
+                                        Descrição</th>
+                                    <th
+                                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">
+                                        Preço</th>
+                                    <th
+                                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">
+                                        Categoria</th>
+                                    <th
+                                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">
+                                        Estoque</th>
+                                    <th
+                                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">
+                                        Ações</th>
                                 </tr>
                             </thead>
-                            <tbody class="bg-white dark:bg-gray-800 divide-y divide-gray-200 dark:divide-gray-700">
+                            <tbody class="bg-white dark:bg-gray-600 divide-y divide-gray-200 dark:divide-gray-500">
                                 @foreach($produtos as $produto)
-                                    <tr>
-                                        <td class="px-6 py-4 whitespace-nowrap">{{ $produto->codigo }}</td>
-                                        <td class="px-6 py-4 whitespace-nowrap">{{ $produto->descricao }}</td>
-                                        <td class="px-6 py-4 whitespace-nowrap">{{ number_format($produto->preco, 2, ',', '.') }}</td>
-                                        <td class="px-6 py-4 whitespace-nowrap">{{ $produto->categoria }}</td>
-                                        <td class="px-6 py-4 whitespace-nowrap">{{ $produto->fornecedor }}</td>
-                                        <td class="px-6 py-4 whitespace-nowrap">{{ number_format($produto->peso, 3, ',', '.') }}</td>
-                                        <td class="px-6 py-4 whitespace-nowrap">{{ number_format($produto->preco_compra, 2, ',', '.') }}</td>
-                                        <td class="px-6 py-4 whitespace-nowrap">{{ number_format($produto->estoque, 3, ',', '.') }}</td>
-                                        <td class="px-6 py-4 whitespace-nowrap">
-                                            <a href="{{ route('oryon.edit', $produto->id) }}" class="text-indigo-600 hover:text-indigo-900">Editar</a>
-                                            <a href="{{ route('oryon.show', $produto->id) }}" class="text-indigo-600 hover:text-indigo-900 ml-2">Detalhes</a>
+                                    <tr
+                                        class="{{ $loop->odd ? 'bg-gray-50 dark:bg-gray-800' : 'bg-gray-100 dark:bg-gray-700' }}">
+                                        <td class="px-6 py-4 whitespace-normal">{{ $produto->codigo }}</td>
+                                        <td class="px-6 py-4 whitespace-normal">{{ $produto->descricao }}</td>
+                                        <td class="px-6 py-4 whitespace-normal">
+                                            {{ 'R$ ' . number_format($produto->preco, 2, ',', '.') }}
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-normal">{{ $produto->categoria }}</td>
+                                        <td class="px-6 py-4 whitespace-normal">
+                                            {{ number_format($produto->estoque, 0, ',', '.') }}
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-normal">
+                                            <a href="{{ route('oryon.edit', $produto->id) }}"
+                                                class="text-indigo-600 hover:text-indigo-900">Editar</a>
+                                            <a href="{{ route('oryon.show', $produto->id) }}"
+                                                class="text-indigo-600 hover:text-indigo-900 ml-2">Detalhes</a>
                                         </td>
                                     </tr>
                                 @endforeach
                             </tbody>
                         </table>
                     </div>
+                    <br>
                     {{$produtos->links()}}
                 </div>
             </div>
