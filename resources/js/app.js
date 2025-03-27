a7ef2d966b5011a0dc4cc11fdbd54777f31d5629
Lucas Garssa <garssa.lucas@gmail.com>
2025-03-27 14:24:59 -0300

    InstallVueJs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,13 @@
 import './bootstrap';
+import '../css/app.css';
 
 import Alpine from 'alpinejs';
+import { createApp } from 'vue';
+import App from './App.vue';
 
 window.Alpine = Alpine;
-
 Alpine.start();
+
+// Inicializa o Vue e o anexa ao #app
+const app = createApp(App);
+app.mount('#app');
